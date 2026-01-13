78c98955623d20571df24ccb7a9d72850d4316af
jmarinr <jmarinr@users.noreply.github.com>
2026-01-13 06:00:52 -0600

    Change base path in Vite configuration

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/auto-inspections/',
+  base: '/',
   build: {
     outDir: 'dist',
     sourcemap: false,
